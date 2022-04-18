83e28154488fba8719d4be47c5181d7ebc0d1135
Elena <lena.bodrenok@gmail.com>
2022-04-18 21:37:51 +0300

    add allure report

--- src/test/java/guru/qa/SelenideTest.java
+++ src/test/java/guru/qa/SelenideTest.java
@@ -1,5 +1,7 @@
 package guru.qa;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
@@ -17,6 +19,8 @@ public class SelenideTest {
 
     @Test
     public void githubIssueTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
         open("https://github.com");
         $(".header-search-input").click();
         $(".header-search-input").sendKeys("eroshenkoam/allure-example");
